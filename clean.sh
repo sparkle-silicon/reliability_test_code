d1955aff5f592ba39342c6bd39862199e4454373
daweslinyu <daowes.ly@qq.com>
2024-10-19 10:37:01 +0800

    modified:   clean.sh 	modified:   crypto_sys

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@
  # @Author: daweslinyu daowes.ly@qq.com
  # @Date: 2024-07-19 15:14:28
  # @LastEditors: daweslinyu daowes.ly@qq.com
- # @LastEditTime: 2024-09-02 14:35:08
+ # @LastEditTime: 2024-10-14 11:37:29
  # @FilePath: /SPK32AE103/clean.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
@@ -20,3 +20,6 @@ cd ..
 rm -f $(find -name "*.tags")
 rm -f $(find -name "*.bin")
 rm -f $(find -name "*.csv")
+rm -f $(find -name "*.pem")
+rm -f $(find -name "*.der")
+rm -f $(find -name "*.txt")
