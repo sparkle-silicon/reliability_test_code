432f22bc03cae0cfe866517d5d7eae55085c224c
daweslinyu <daowes.ly@qq.com>
2025-09-12 18:02:16 +0800

    优化工具打印 	modified:   Firmware/TOOLS/app/efuse_crc32_value 	modified:   Firmware/TOOLS/app/efuse_crc32_value.c

--- Firmware/TOOLS/app/efuse_crc32_value.c
+++ Firmware/TOOLS/app/efuse_crc32_value.c
@@ -136,12 +136,12 @@ int main(int argc, char **argv)
     {
         // printf("GEt %s of EFUSE_WD[0]\n", argv[1]);
         input_val[0] = strtol(argv[1], NULL, 16);
-        printf("GEt %#x of EFUSE_WD[0]\n", input_val[0]);
+        printf("GET %#x of EFUSE_WD[0]\n", input_val[0]);
         if(argc >= 3)
         {
             // printf("GEt %s of EFUSE_WD[1]\n", argv[2]);
             input_val[1] = strtol(argv[2], NULL, 16);
-            printf("GEt %#x of EFUSE_WD[0]\n", input_val[1]);
+            printf("GET %#x of EFUSE_WD[1]\n", input_val[1]);
         }
         data = input_val;
 
