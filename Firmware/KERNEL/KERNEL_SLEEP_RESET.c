dfdede60b5615b013968ad60b0e494670b550e37
Iversu <coolwangzi@126.com>
2025-12-02 18:44:00 +0800

    删除通知子系统复位alg/trng的接口，改为由子系统在flash退出powerdown状态时直接复位alg/trng模块
    Signed-off-by: Iversu <coolwangzi@126.com>

--- Firmware/KERNEL/KERNEL_SLEEP_RESET.c
+++ Firmware/KERNEL/KERNEL_SLEEP_RESET.c
@@ -74,25 +74,6 @@ void Module_SoftReset(int reg_idx, int bit_no)
 }
 
 /* ----------------------------------------------------------------------------
- * FUNCTION:   Reset_ALG_TRNG_AfterWakeup
- *
- * you must notice crypto system to reset alg and trng module when wake up from low power
- *
- * ------------------------------------------------------------------------- */
-void Reset_ALG_TRNG_AfterWakeup(void)
-{
-	SYSCTL_DVDD_EN |= BIT(11);//alg dvdd enable
-	nop; nop; nop; nop; nop; nop;
-	MAILBOX_SELF_CMD = 0xE0; // 命令字
-	MAILBOX_SET_IRQ(MAILBOX_Control_IRQ_NUMBER);   // 触发子系统中断
-	MAILBOX_WAIT_IRQ(0xE0, MAILBOX_Control_IRQ_NUMBER);
-	MAILBOX_CLEAR_IRQ(MAILBOX_Control_IRQ_NUMBER); // 清除中断状态
-	nop; nop; nop; nop; nop; nop;
-	SYSCTL_DVDD_EN &= ~((BIT(11)) << 12);//alg iso enable
-	nop; nop; nop; nop; nop; nop;
-}
-
-/* ----------------------------------------------------------------------------
  * FUNCTION:   CPU_SLP_RES
  *
  * This is CPU Sleep and Reset Interface,If you call this function,cpu will sleep and
@@ -309,7 +290,6 @@ void Enter_LowPower_Mode(void)
 	Smf_Ptr = Load_Smfi_To_Dram(CPU_SLP_RES, 0x600);
 	if (Smf_Ptr != 0)
 		(*Smf_Ptr)(); // Do Function at malloc address
-	Reset_ALG_TRNG_AfterWakeup();//唤醒后通知子系统复位alg/trng模块，以免算法出错
 	Enable_Interrupt_Main_Switch();
 	free(Smf_Ptr);  // 释放通过 malloc 分配的内存空间
 	Smf_Ptr = NULL; // 将指针设置为 NULL，以避免悬空指针问题
